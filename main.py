92a770e5669ad476920e04dd8a9967b5e632bc4b
bunturioto <Bunturioto@gmail.com>
2024-03-26 13:57:48 +0400

    Loop over each filename to create a thread for parsing JSON data

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import json
 import threading
 
 
+# Function to parse JSON data from a file
 def parse_json(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
         print(f"Data from {filename}: {data}")
+
+
+# List of filenames containing JSON data
+filenames = ["file1.json", "file2.json", "file3.json"]
+
+# List to store thread objects
+threads = []
+
+# Loop over each filename to create a thread for parsing JSON data
+for filename in filenames:
+    thread = threading.Thread(target=parse_json, args=(filename,))
+    threads.append(thread)
+    thread.start()
